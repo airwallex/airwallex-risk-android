0e70f059884e9df7580718c2e88fc6805d4b09d9
Jarrod Robins <jarrod.robins@airwallex.com>
2023-07-31 16:30:31 +1000

    chore: fixed update versions script

--- .github/scripts/update-versions.sh
+++ .github/scripts/update-versions.sh
@@ -8,6 +8,6 @@ fi
 
 echo "Updating version to $VERSION."
 
-find . -type f -name "gradle.properties" | xargs sed -i '' -e 's/\(airwallexSdkVersion*="\)[^"]*/\1'"$VERSION"'/g'
-find . -type f -name "README.md" | xargs sed -i '' -e 's/\(''io.github.airwallex:risk-sdk:*\).*/\1'"$VERSION"'''/g'
-sed -i '' -e 's/\(airwallexSdkVersion = '\''\)[^'\'']*/\1'"$VERSION"'/g' ./build.gradle
+find . -type f -name "gradle.properties" | xargs sed -i 's/\(airwallexSdkVersion*="\)[^"]*/\1'"$VERSION"'/g'
+find . -type f -name "README.md" | xargs sed -i 's/\("io.github.airwallex:risk-sdk:*\)[^"]*/\1'"$VERSION"'''/g'
+sed -i 's/\(airwallexSdkVersion = '\''\)[^'\'']*/\1'"$VERSION"'/g' ./build.gradle

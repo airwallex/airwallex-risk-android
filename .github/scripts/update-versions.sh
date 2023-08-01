6b642339be044e5fde65056ac4901027442562e9
Jarrod Robins <jarrod.robins@airwallex.com>
2023-08-01 11:37:43 +1000

    chore: update version bump script

--- .github/scripts/update-versions.sh
+++ .github/scripts/update-versions.sh
@@ -9,5 +9,5 @@ fi
 echo "Updating version to $VERSION."
 
 find . -type f -name "gradle.properties" | xargs sed -i 's/\(airwallexSdkVersion*="\)[^"]*/\1'"$VERSION"'/g'
-find . -type f -name "README.md" | xargs sed -i 's/\("io.github.airwallex:risk-sdk:*\)[^"]*/\1'"$VERSION"'''/g'
+find . -type f -name "README.md" | xargs sed -i 's/\("io.github.airwallex:RiskSdk:*\)[^"]*/\1'"$VERSION"'''/g'
 sed -i 's/\(airwallexSdkVersion = '\''\)[^'\'']*/\1'"$VERSION"'/g' ./build.gradle
